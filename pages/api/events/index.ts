bada2a0852e6892cfed9803dace3b1f131f52c72
cadetjer <cadet.j@northeastern.edu>
2023-01-18 14:53:04 -0500

    Pushed at "API Routes"

---
+++ pages/api/events/index.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// } for TypeScript
+
+const {events} = require('./data.json')
+
+export default (req:any, res:any) => {
+  if(req.method === 'GET') {
+    res.status(200).json(events)
+  }
+  else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).json({message: `Method ${req.method} is not allowed`})
+  }
+}
