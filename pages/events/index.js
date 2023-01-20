e32369bcb5c993258ef0a9de134ac989e928d2f6
cadetjer <cadet.j@northeastern.edu>
2023-01-20 15:12:35 -0500

    fetching count gives error 404

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,11 @@
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
+import Pagination from '@/components/Pagination'
 import {API_URL} from '@/config/index'
+const PER_PAGE = 5
 
-export default function EventPage({events}) {
+export default function EventPage({events, page, total}) {
+  const lastPage = Math.ceil(total / PER_PAGE)
   return (
     <Layout>
       <h1>Events</h1>
@@ -11,17 +14,28 @@ export default function EventPage({events}) {
       {events.data.map((evt) => (
       <EventItem key={evt.id} evt={evt.attributes} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events?populate=*&?_sort=date:ASC`)
-  const events = await res.json()
+export async function getServerSideProps({query: {page = 1}}) {
+  //Calculate start page
+  const start = +page === 1 ? 0 : (+page-1) * PER_PAGE
 
+  //Fetch total/count
+  const totalRes = await fetch(`${API_URL}/events/count`)
+  const total = await totalRes.json()
+
+  //Fetch events
+
+  const eventRes = await fetch(`${API_URL}/api/events?populate=*&?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`)
+  //?populate=*&
+  const events = await eventRes.json()
+   
   return {
-    props: {events},
-    revalidate: 1,
+    props: {events, page: +page, total},
   }
 }
 
