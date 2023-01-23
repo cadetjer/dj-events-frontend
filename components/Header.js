b30f266d40b618ab0690b19f2442e8280674d562
cadetjer <cadet.j@northeastern.edu>
2023-01-23 15:30:18 -0500

    Connection to backend doesnt work still

--- components/Header.js
+++ components/Header.js
@@ -1,40 +1,66 @@
-import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
-import Link from "next/link";
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { useContext } from 'react'
+import Link from 'next/link'
 import Search from './Search'
+import AuthContext from '@/context/AuthContext'
 import styles from '@/styles/Header.module.css'
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <header className={styles.header}>
-        <div className={styles.logo}>
-            <Link href='/'>
-                DJ Events
+      <div className={styles.logo}>
+        <Link legacyBehavior href='/'>
+          <a>DJ Events</a>
+        </Link>
+      </div>
+
+      <Search />
+
+      <nav>
+        <ul>
+          <li>
+            <Link legacyBehavior href='/events'>
+              <a>Events</a>
             </Link>
-        </div>
-
-        <Search />
-
-        <nav>
-            <ul>
-                <li>
-                    <Link href='/events'>
-                        Events
-                    </Link>
-                </li>
-                <li>
-                    <Link legacyBehavior href='/events/add'>
-                        <a>Add Event</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link legacyBehavior href='/account/login'>
-                        <a className="btn-secondary btn-icon">
-                            <FaSignInAlt /> Login
-                        </a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+          </li>
+          {user ? (
+            // If logged in
+            <>
+              <li>
+                <Link legacyBehavior href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link legacyBehavior href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className='btn-secondary btn-icon'
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // If logged out
+            <>
+              <li>
+                <Link legacyBehavior href='/account/login'>
+                  <a className='btn-secondary btn-icon'>
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+import {AuthProvider} from '@/context/AuthContext'
 // import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AuthProvider>
+    <Component {...pageProps} />
+    </AuthProvider>
 }
